a8d809ed527ce23285b244a9a9a8f4c1c108af68
Dang Khoa <hnkhoa04@gmail.com>
2025-02-22 10:06:59 +0700

    init

---
+++ demo/src/test/java/java_spring_laptopshop/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package java_spring_laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
