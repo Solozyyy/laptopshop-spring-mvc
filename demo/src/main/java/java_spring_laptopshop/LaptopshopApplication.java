23cf047c6ef58c7c32bfa0f50df42545fd0459f9
Dang Khoa <hnkhoa04@gmail.com>
2025-02-25 08:11:23 +0700

    update enable spring security

--- demo/src/main/java/java_spring_laptopshop/LaptopshopApplication.java
+++ demo/src/main/java/java_spring_laptopshop/LaptopshopApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
 // exclude: loai bo auto config phan nao ra >< include: auto config phan nao
-// @SpringBootApplication(exclude =
-// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+		org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 
 public class LaptopshopApplication {
 
