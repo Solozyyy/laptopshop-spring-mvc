8432cd5b42141ddba7bedee28de44bbb9acaad34
Dang Khoa <hnkhoa04@gmail.com>
2025-03-09 23:24:31 +0700

    create relation between order and product => orderDetail N-N relation => convert to N-1 + 1-N

--- demo/src/main/java/java_spring_laptopshop/domain/Order.java
+++ demo/src/main/java/java_spring_laptopshop/domain/Order.java
@@ -1,11 +1,14 @@
 package java_spring_laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderDetail> orderDetails;
 
     public Order(long id, long totalPrice) {
         this.id = id;
@@ -41,6 +47,22 @@ public class Order {
 
     public void setTotalPrice(long totalPrice) {
         this.totalPrice = totalPrice;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(List<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
     }
 
     @Override
