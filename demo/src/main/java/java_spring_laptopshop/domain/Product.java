ff066f66dfb615e08fca3f0c3299c6c218f21d66
Dang Khoa <hnkhoa04@gmail.com>
2025-03-09 10:45:54 +0700

    User annotation entity to create table in db

--- demo/src/main/java/java_spring_laptopshop/domain/Product.java
+++ demo/src/main/java/java_spring_laptopshop/domain/Product.java
@@ -1,6 +1,17 @@
 package java_spring_laptopshop.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
+
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private double price;
