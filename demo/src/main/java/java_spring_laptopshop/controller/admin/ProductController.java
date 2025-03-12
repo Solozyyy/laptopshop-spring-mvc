91c974cc24a0ca108e05529e828ef6c5626ef967
Dang Khoa <hnkhoa04@gmail.com>
2025-03-12 07:33:30 +0700

    Create interface for admin (clone from bootstrap sb-admin-ghpage)

---
+++ demo/src/main/java/java_spring_laptopshop/controller/admin/ProductController.java
@@ -0,0 +1,13 @@
+package java_spring_laptopshop.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+    @GetMapping("/admin/product")
+    public String getProducts() {
+        return "/admin/product/show";
+    }
+
+}
